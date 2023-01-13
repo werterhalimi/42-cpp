8f6bcef070ed5763e650aa24a1e61a9d7a67e87a
Shai <shai.halimi@gmail.com>
2023-01-14 00:23:45 +0100

    added default destructor

--- cpp03/ex01/inc/ScavTrap.hpp
+++ cpp03/ex01/inc/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: shalimi <shalimi@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:04:45 by shalimi           #+#    #+#             */
-/*   Updated: 2023/01/14 00:15:59 by shalimi          ###   ########.fr       */
+/*   Updated: 2023/01/14 00:23:21 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 class ScavTrap : public ClapTrap
 {
 	public:
-		ScavTrap (void) : ClapTrap("Jerem", 100, 50, 20) { std::cout << "Default ScavTrap constructor" << std::endl;}
+		ScavTrap ( void ) : ClapTrap("Jerem", 100, 50, 20) { std::cout << "Default ScavTrap constructor" << std::endl;}
+		~ScavTrap ( void ) { std::cout << "Default ScavTrap destructor" << std::endl;}
 		void guardGate( void ) { std::cout << this->getName() << " has entered GATE KEEPER MODE" << std::endl;}
 };
 #endif

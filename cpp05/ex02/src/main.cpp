907c7bc1b9ada951a056211d1506b506733555df
Shai Halimi <shalimi@c2r7s7.42lausanne.ch>
2023-01-18 18:27:16 +0100

    done some test to show everything is going well

--- cpp05/ex02/src/main.cpp
+++ cpp05/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shalimi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 19:38:18 by shalimi           #+#    #+#             */
-/*   Updated: 2023/01/18 01:21:49 by shalimi          ###   ########.fr       */
+/*   Updated: 2023/01/18 18:26:41 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,25 @@
 
 int	main(void)
 {
-	Bureaucrat b("Shai", 1);
-	ShrubberyCreationForm f("ShrubberyCreationForm 0", "test");
-	b.executeForm(f);
+	{
+		Bureaucrat b("Shai", 1);
+		ShrubberyCreationForm f("ShrubberyCreationForm 0", "test");
+		b.executeForm(f);
+	}
+	{
+		Bureaucrat b("Shai", 1);
+		PresidentialPardonForm f("ShrubberyCreationForm 0", "test");
+		b.executeForm(f);
+	}
+	{
+		Bureaucrat b("Shai", 1);
+		RobotomyRequestForm f("ShrubberyCreationForm 0", "test");
+		b.executeForm(f);
+	}
+	{
+		Bureaucrat b("Shai", 150);
+		ShrubberyCreationForm f("ShrubberyCreationForm 0", "test");
+		b.executeForm(f);
+	}
+
 }

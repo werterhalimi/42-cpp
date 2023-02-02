58c615cb6b15288b7287ee47fd5f71f6193f7c5c
Shai <shai.halimi@gmail.com>
2023-02-02 20:28:45 +0100

    finished ex01

---
+++ cpp06/ex01/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shalimi <shalimi@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 20:08:43 by shalimi           #+#    #+#             */
+/*   Updated: 2023/02/02 20:28:16 by shalimi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "data.h"
+
+uintptr_t serialize(Data * ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data * deserialize(uintptr_t ptr)
+{
+	return reinterpret_cast<Data *>(ptr);
+}
+
+int	main(void)
+{
+	Data d;
+	d.data = 10;
+	uintptr_t s = serialize(&d);
+	Data * u = deserialize(s);
+	std::cout << u->data << std::endl;
+}

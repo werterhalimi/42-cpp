05b98e827f722b790a9d68a3c0e8ddf8f4afaa51
Shai <shai.halimi@gmail.com>
2023-02-03 01:55:59 +0100

    finished ex02

---
+++ cpp06/ex02/inc/base.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   base.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shalimi <shalimi@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 00:46:15 by shalimi           #+#    #+#             */
+/*   Updated: 2023/02/03 01:50:50 by shalimi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_H
+# define BASE_H
+# include <iostream>
+# include <cstdlib>
+
+class Base
+{
+	public:
+		virtual ~Base(void);
+};
+
+class A : public Base{};
+class B : public Base{};
+class C : public Base{};
+
+#endif
